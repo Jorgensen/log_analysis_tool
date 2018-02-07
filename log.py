b7aa22dac7787e767643d68eb97154e490551933
jorgensen <www1636601@gmail.com>
2018-02-07 21:11:52 +0800

    modify by udacity reviewr's suggestion

--- log.py
+++ log.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+This is a reporting tool.
+
+It prints out reports (in plain text) based on the data in the database.
+It is a Python program using the psycopg2 module to connect to the database.
+"""
+
 import psycopg2
 import time
 
@@ -5,54 +15,41 @@ DBNAME = "news"
 
 db = psycopg2.connect(database=DBNAME)
 c = db.cursor()
-c.execute("""select title, count(*) as number from log, articles
-where log.status = '200 OK' and articles.slug = replace(path, '/article/', '')
-group by title
-order by number desc
-limit 3;""")
-popular_article = c.fetchall() 
+c.execute("""SELECT title, COUNT(*) AS number
+FROM log, articles
+WHERE log.status = '200 OK' AND articles.slug = replace(path, '/article/', '')
+GROUP BY title
+ORDER BY number DESC
+LIMIT 3;""")
+popular_article = c.fetchall()
 
-c.execute("""select name, count(*) as number from log, articles, authors
-where log.status = '200 OK' and articles.slug = replace(path, '/article/', '') and articles.author = authors.id
-group by name
-order by number desc
-limit 3;""")
-popular_author = c.fetchall() 
+c.execute("""SELECT name, COUNT(*) AS number
+FROM log, articles, authors
+WHERE log.status = '200 OK' AND articles.slug = replace(path, '/article/', '')
+AND articles.author = authors.id
+GROUP BY name
+ORDER BY number DESC
+LIMIT 3;""")
+popular_author = c.fetchall()
 
-c.execute("""create view v1 as select time::date as days, count(*) as number from log 
-group by days
-;
+c.execute("""CREATE VIEW v1 AS SELECT time::DATE AS days, COUNT(*) AS number
+FROM log
+GROUP BY days;
 """)
 
-c.execute("""create view v2 as select time::date as day, count(*) as num from log
-where status != '200 OK'
-group by day
-;
+c.execute("""CREATE VIEW v2 AS SELECT time::DATE AS day, COUNT(*) AS num
+FROM log
+WHERE status != '200 OK'
+GROUP BY day;
 """)
 
-c.execute("""select day, round(round(num::numeric/number::numeric, 4)*100,2) from v1, v2
-where v1.days = v2.day and round(v2.num::numeric/v1.number::numeric, 4) > 0.01
-group by day, v2.num, v1.number
-;
+c.execute("""SELECT day, ROUND(ROUND(num::NUMERIC/number::NUMERIC, 4)*100,2)
+FROM v1, v2
+WHERE v1.days = v2.day AND ROUND(v2.num::NUMERIC/v1.number::NUMERIC, 4) > 0.01
+GROUP BY day, v2.num, v1.number;
 """)
-errors = c.fetchall() 
+errors = c.fetchall()
 
-# print "==============================================="
-# print "The most popular three articles of all time: "
-# for v in popular_article:
-#     print " ",v[0],"--", v[1],"views"
-# 
-# 
-# print "==============================================="
-# print "The most popular article authors of all time: "
-# for v in popular_author:
-#     print " ",v[0],"--", v[1],"views"
-# 
-# 
-# print "==============================================="
-# print "Days more than 1% of requests lead to errors: "
-# for v in errors:
-#     print " ",v[0],"--", v[1],"% errors"
 
 fo = open("report.txt", "wb")
 fo.write("\n===============================================\n")
